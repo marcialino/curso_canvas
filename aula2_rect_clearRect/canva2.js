37366f75305b259351bdc04077218187d6048aa2
marcialino <marcialino100@gmail.com>
2023-06-12 11:08:56 -0300

    atualizei

--- aula2_rect_clearRect/canva2.js
+++ aula2_rect_clearRect/canva2.js
@@ -1,5 +1,19 @@
+
+var px= 50 /*posição x*/ 
+var py= 50 /*posição y*/
 var canvas = document.getElementById('desenho1')
 var ctx = canvas.getContext('2d')
 
-ctx.fillStyle = '#00f'
-ctx.fillRect(0,0,100,100)
+/*
+ctx.fillRect = desenha um retângulo preenchido
+ctx.rect: só desenha o quadrado, mas não preenche
+ctx.fill: preenche o quadrado desenhado acima.
+ctx.clearRect: apaga a cor da área especificada.
+*/ 
+
+ctx.fillStyle ='#00f'
+ctx.rect(px,py,200,200)
+ctx.fill()
+
+ctx.clearRect(100, 100, 100, 100)
+
