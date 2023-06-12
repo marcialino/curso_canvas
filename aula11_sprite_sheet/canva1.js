20b259d5c5abceb81f13605221cae84b2c62aa82
marcialino <marcialino100@gmail.com>
2023-06-12 10:24:48 -0300

    criei aula 1 canvas

---
+++ aula11_sprite_sheet/canva1.js
@@ -0,0 +1,11 @@
+var canvas= document.getElementById('desenho1')
+var ctx = canvas.getContext('2d')
+
+/*ctx.fillStyle: determina a cor pu o estilo.
+ctx.fillRect: determina onde o estilo será aplicado no canvas.
+*/ 
+ctx.fillStyle= '#00F'
+ctx.fillRect(0,0,100,100)
+
+ctx.fillStyle = 'rgba(255, 0, 0, 0.5'/*0.5 é opacidade*/
+ctx.fillRect(50, 50, 100, 100)
