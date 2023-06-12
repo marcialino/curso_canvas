c5e3f08f0ffbe4cb0e96e61090e3e2a258124177
marcialino <marcialino100@gmail.com>
2023-06-12 13:54:31 -0300

    atualizei

--- aula3_linhas/canvas3.js
+++ aula3_linhas/canvas3.js
@@ -7,6 +7,8 @@ ctx.moveTo(0,0)
 ctx.lineTo(250,250) /*moveu a linha do 0para 250, tanto no eixo x, quanto no eixo y*/
 ctx.lineTo(400,250)
 ctx.lineTo(400, 150)
+ctx.moveTo(250,250) /*volta o início da linha para o centro*/
+ctx.lineTo(200,300)
 ctx.stroke() /*para preencher a linha*/
 
 
