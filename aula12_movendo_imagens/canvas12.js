437e0a5ae4b79a94740bed21406e8068e08627bf
marcialino <marcialino100@gmail.com>
2023-06-16 19:01:28 -0300

    atualizei

--- aula12_movendo_imagens/canvas12.js
+++ aula12_movendo_imagens/canvas12.js
@@ -3,7 +3,7 @@ let canvas = document.getElementById('desenho1')
 let ctx = canvas.getContext('2d')
 
 let jogador= new Image()
-jogador.src="imagens/mario.gif"
+jogador.src="imagens/nave.png"
 
 let numSprite =0
 let posInix =0
@@ -32,34 +32,34 @@ const desenha=()=>{
 }
 
 window.addEventListener('keydown',(event)=>{
-    if(KeyboardEvent.keyCode==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
+    if(event.key==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
         esquerda=true
 
-    }else if(KeyboardEvent.keyCode==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
+    }else if(event.key==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
         direita=true
     }
 
-    if(KeyboardEvent.keyCode==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
+    if(event.key==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
         cima=true
 
-    }else if(KeyboardEvent.keyCode==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
+    }else if(event.key==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
         baixo=true
     }
 
 })
 
 window.addEventListener('keyup',(event)=>{
-    if(KeyboardEvent.keyCode==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
+    if(event.key==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
         esquerda=false
 
-    }else if(KeyboardEvent.keyCode==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
+    }else if(event.key==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
         direita=false
     }
 
-    if(KeyboardEvent.keyCode==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
+    if(event.key==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
         cima=false
 
-    }else if(KeyboardEvent.keyCode==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
+    }else if(event.key==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
         baixo=false
     }
 
---
+++ aula13_imagens/jogador.js
@@ -0,0 +1,36 @@
+class Jogador{
+    constructor(ctx,teclado){
+        this.ctx=ctx
+        this.teclado=teclado
+        this.x=0
+        this.y=0
+        this.vel=0
+        this.nave=new Image()
+        this.nave.src="imagens/nave.png"
+
+        this.nave.addEventListener('load',()=>{
+           desenhar()
+        })
+
+    }
+    gerenciar(){
+        if(this.teclado.esquerda){
+            this.x-=this.vel
+        }
+        if(this.teclado.direita){
+            this.x+=this.vel
+        }
+        if(this.teclado.cima){
+            this.y-=this.vel
+        }
+        if(this.teclado.baixo){
+            this.y+=this.vel
+        }
+
+    }
+    desenhar(){
+        this.ctx.drawImage(this.nave, this.x, this.y)
+        this.gerenciar()
+    }
+}
+
