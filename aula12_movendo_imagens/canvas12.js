52d81fad89fff7357c29cf93da769641e888c6d9
marcialino <marcialino100@gmail.com>
2023-06-17 08:40:06 -0300

    atualizei

--- aula12_movendo_imagens/canvas12.js
+++ aula12_movendo_imagens/canvas12.js
@@ -1,91 +1,64 @@
 let canvas = document.getElementById('desenho1')
-
 let ctx = canvas.getContext('2d')
-
 let jogador= new Image()
 jogador.src="imagens/nave.png"
 
-let numSprite =0
-let posInix =0
-let Limagem = 0
-let largSprite =0
-let numSpries = 3
-let posX = 0
-let posY = 0
-let velocidade = 5
-let cima=false, baixo=false, esquerda=false,direita=false
+let dx=5 
+let dy=5
+let x = 250
+let y = 100
+let WIDTH = 500
+let HEIGHT = 500
 
-jogador.addEventListener('load', ()=>{
-  
-    largSprite = jogador.width/numSpries
-    altSprite = jogador.height/numSpries
-    posInix = largSprite*numSprite
-    ctx.drawImage(jogador, posInix,0,largSprite, altSprite,posX,posY,largSprite, altSprite)
-})
+function Iniciar(){
+    let canvas = document.getElementById('desenho1')
+    
+    let ctx = canvas.getContext('2d')
+    
+    return setInterval(Atualizar, 10)
+    }
 
-
-const desenha=()=>{
+const desenhar=()=>{
     ctx.clearRect(0, 0, 500, 500);
     /*ctx.drawImage(jogador, posInix, 0,largSprite, altSprite, posX,posY, largSprite, altSprite)*/
 
     ctx.drawImage(jogador, 120,80, 100, 200); /*desenhar a imagem, nas posições x e y*. Nas dimensões largura:200 e altura: 300*/
 }
 
-window.addEventListener('keydown',(event)=>{
-    if(event.key==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
-        esquerda=true
 
-    }else if(event.key==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
-        direita=true
-    }
+window.addEventListener('keyDown',(event)=>{
 
-    if(event.key==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
-        cima=true
-
-    }else if(event.key==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
-        baixo=true
-    }
-
+    switch (evt.keyCode) {
+        case 38:  /*seta para cima */
+            if (y - dy > 0){
+                y -= dy;
+            }
+            break;
+        case 40:  /*set para baixo*/
+            if (y + dy < HEIGHT){
+                y += dy;
+            }
+            break;
+        case 37:  /*set para esquerda*/
+            if (x - dx > 0){
+                x -= dx;
+            }
+            break;
+        case 39:  /*seta para direita*/
+            if (x + dx < WIDTH){
+                x += dx;
+            }
+            break;
 })
 
-window.addEventListener('keyup',(event)=>{
+
+/*window.addEventListener('keydown',(event)=>{
     if(event.key==37){ /*se o apertar a tecla 37 que é seta à esquerda, a posição de x vai uma posição à esquerda*/
-        esquerda=false
 
-    }else if(event.key==39){ /*se o apertar a tecla 39 que é seta à direita, a posição de x vai uma posição à direita*/
-        direita=false
-    }
-
-    if(event.key==38){ /*se o apertar a tecla 38 que é seta para cima, a posição de x vai uma posição para cima*/
-        cima=false
-
-    }else if(event.key==40){ /*se o apertar a tecla 40 que é seta para baixo, a posição de x vai uma posição para baixo*/
-        baixo=false
-    }
-
-})
-
-const game =()=>{
-    if(cima){
-        posY-=velocidade
-    }
-    if(baixo){
-        posY+=velocidade
-    }
-    if(esquerda){
-        posX-=velocidade
-    }
-    if(direita){
-        posX+=velocidade
-    }
-
- 
-    requestAnimationFrame(game)
-    desenha()
+function Atualizar(){
+  desenhar()
 }
 
-requestAnimationFrame(game)
-
-
-  
-
+window.addEventListener('keyDown', KeyDown, true)
+Iniciar()
+       
