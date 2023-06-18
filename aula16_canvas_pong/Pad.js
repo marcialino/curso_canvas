474d7f9413bd08233b5c4fb70141b39576dbb9e2
marcialino <marcialino100@gmail.com>
2023-06-18 19:11:22 -0300

    atualizei

--- aula16_canvas_pong/Pad.js
+++ aula16_canvas_pong/Pad.js
@@ -3,13 +3,16 @@ class Pad{
     constructor(ctx,teclado){
         this.ctx=ctx
         this.teclado=teclado
-        this.x=0
-        this.y=0
         this.vel=3
         this.largura=20
         this.altura=100
+        this.x=0
+        this.y= (this.ctx.canvas.height/2)-this.altura
         
-        
+        window.addEventListener('load',()=>{
+            this.desenhar()
+        })
+                
     }
     atualizar(){
         
@@ -18,7 +21,7 @@ class Pad{
             this.x-= this.vel
         }
         if(this.teclado.direita){
-             if(this.x < this.ctx.canvas.width-this.largura)
+             if(this.x < (this.ctx.canvas.width/2)-this.largura)
             this.x+=this.vel
         }
         if(this.teclado.cima){
@@ -34,12 +37,8 @@ class Pad{
     desenhar(){
         this.atualizar()
         this.ctx.fillStyle='#00f'  
-        this.ctx.rect= (this.x, this.y, this.largura, this.altura)
-        this.ctx.clearRect(100,100,100,100)
-        
-  
-          
-        
+        this.ctx.fillRect=(this.x, this.y, this.largura, this.altura)
+                
     }
     
 }
