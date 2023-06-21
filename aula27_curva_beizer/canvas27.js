fce6e80cf9e9f4e9da03e85e9c286da128bdcb5a
marcialino <marcialino100@gmail.com>
2023-06-21 15:41:36 -0300

    atualizei

--- aula27_curva_beizer/canvas27.js
+++ aula27_curva_beizer/canvas27.js
@@ -3,13 +3,22 @@ let canvas = document.getElementById('desenho1')
 let ctx = canvas.getContext('2d')
 
 ctx.beginPath()
-ctx.lineTo(200, 100)
+ctx.lineTo(300, 100)
 ctx.fillStyle='#00f'
-ctx.quadraticCurveTo(300, 0, 400, 100)
-ctx.quadraticCurveTo(500, 200, 400, 320)
-ctx.quadraticCurveTo(320,420,200,500)
+/*lado direito do coração*/ 
+ctx.quadraticCurveTo(400, 0, 500, 100)
+ctx.quadraticCurveTo(600, 200, 500, 320)
+ctx.quadraticCurveTo(420,420,300,500)
+/*Lado esquerdo do coração. Ele é espelhado. Para calcular as coordenadas:
+próximo ponto:
+xdif=xanterior-xatual (420-300 = 120)
+xpróximo = xatual-xdif (300-120 = 180)
+próxima coordenada = (180,420)
+*/ 
+ctx.quadraticCurveTo(180,420,100,320)
+ctx.quadraticCurveTo(0,200,100,100)
+ctx.quadraticCurveTo(200,0, 300,100)
 ctx.fill()
 ctx.stroke();
 ctx.closePath()
-
 
