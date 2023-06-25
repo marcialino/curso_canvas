483839f6649e35440e73a3cfd398d15e32e32e8e
marcialino <marcialino100@gmail.com>
2023-06-25 19:02:56 -0300

    atualizei

--- aula43_sprite_sheet/canvas43.js
+++ aula43_sprite_sheet/canvas43.js
@@ -1,22 +1,34 @@
 let canvas = document.getElementById('desenho1')
 let ctx = canvas.getContext('2d')
 
-let cores = new Image()
-cores.scr="imagens/cores.png"
+let jogador=new Image()
 
+jogador.src="imagens/spritesheetweb.png" 
 let numSprite=0
-let Limagem=0
 let posIniX=0
-let largSprite=0
+let Limagem=0
+let largSprite = 0
 let numSpries=4
 
+jogador.addEventListener('load', ()=>{
 
-cores.addEventListener('load', ()=>{
-    Limagem=cores.width
-    largSprite=Limagem/numSpries
-    posIniX= largSprite*numSprite
+    largSprite = jogador.width/numSpries
+    posIniX = largSprite*numSprite
+    altSprite = jogador.height/numSpries
 
-    ctx.drawImage(cores, posIniX, 0, largSprite,50,0,0,200,200);
+    /*dados para chamar uma imagem:(imagem: XiniRecorte, YinicRecorte, Lrecorte, Arecorte, posX, posY, Limagem, Aimagem*/
+
+    ctx.drawImage(jogador, posIniX, 0,largSprite,altSprite,0,0,largSprite,altSprite);
     
 })
+
+let anima=setInterval(()=>{
+    ctx.clearRect(0, 0, 500, 500);
+    
+    numSprite++
+    if(numSprite>3)
+        numSprite=0
+    posIniX=largSprite*numSprite
+    ctx.drawImage(jogador,posIniX,0,largSprite,altSprite,0,0,largSprite,altSprite)
+},200)
 
